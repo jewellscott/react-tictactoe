262b3cae1a3fe119b531ee604258d854b3e223dd
Jewell Scott <jewelljrscott@gmail.com>
2022-08-08 14:00:12 -0400

    Evaluate winner and add validation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
 import { useState } from 'react';
-import { hasUnreliableEmptyValue } from '@testing-library/user-event/dist/utils';
 
 function App() {
 
   // TODO
-  // add validation so the user can't click on a full slot
+  // add a scoreboard
+  
 
   const initialState = {
-    // it wouild be easier to handle if i made board an array full of objects that had a persistent key instead of an index... i think
     board: ['', '', '', '', '', '', '', '', ''],
     isPlaying: false,
-    message: null
+    message: null,
+    win: false,
+    score: {
+      user: 0,
+      bot: 0
+    }
   }
 
   const [ state, setState ] = useState(initialState)
@@ -20,7 +24,10 @@ function App() {
     let newBoard = state.board;
     newBoard[e.target.dataset.key] = "⨉"
     setState({...state, board: newBoard});
-    evaluateWinner();
+    if (evaluateWinner()) {
+    } else {
+      handleBotTurn();
+    }
   }
 
   function checkIfFull() {
@@ -41,51 +48,46 @@ function App() {
   const handleBotTurn = async () => {
     let newBoard = state.board;
     let boardOccupants = newBoard.map((elem, index) => [elem, index])
-    let empty = boardOccupants.filter(elem => elem[0] == '').map(elem => elem[1]);
+    let empty = boardOccupants.filter(elem => elem[0] === '').map(elem => elem[1]);
     let index = getRandomArray(empty)[0];
     newBoard[index] = "o";
     setState({...state, board: newBoard});
+    evaluateWinner();
   }
 
   // refactor to check for a winner after every human and bot turn
+  // computer wins don't work because the last winning move the computer did isn't registered until the next turn. fix it!
   function evaluateWinner() {
     let newMessage = state.message;
     let newBoard = state.board;
-    // turn the stateboard into a mini board like this?
     const evalBoard = [
-      [newBoard[0], newBoard[1], newBoard[2]],
-      [newBoard[3], newBoard[4], newBoard[5]],
-      [newBoard[6], newBoard[7], newBoard[8]],
-      [newBoard[0], newBoard[3], newBoard[6]],
-      [newBoard[1], newBoard[4], newBoard[7]],
-      [newBoard[2], newBoard[5], newBoard[8]],
-      [newBoard[0], newBoard[4], newBoard[8]],
-      [newBoard[2], newBoard[4], newBoard[6]],
+      [newBoard[0], newBoard[1], newBoard[2]].join(),
+      [newBoard[3], newBoard[4], newBoard[5]].join(),
+      [newBoard[6], newBoard[7], newBoard[8]].join(),
+      [newBoard[0], newBoard[3], newBoard[6]].join(),
+      [newBoard[1], newBoard[4], newBoard[7]].join(),
+      [newBoard[2], newBoard[5], newBoard[8]].join(),
+      [newBoard[0], newBoard[4], newBoard[8]].join(),
+      [newBoard[2], newBoard[4], newBoard[6]].join(),
     ]
 
-    let xArr = ['⨉', '⨉', '⨉'];
-    let oArr = ['o', 'o', 'o'];
-    let winner;
-
     // learned that .includes takes a STRING as a param only
-    if (false) {
-        let newMessage = "You won! Press RESET to play again!"
-        setState({...state, message: newMessage})
-        console.log("user")
-    } else if (winner == "bot") {
-        let newMessage = "You let a dumb computer beat you. That's embarrassing. If your ego can handle it, press RESET to play again."
-        setState({...state, message: newMessage})
+    if (evalBoard.includes('⨉,⨉,⨉')) {
+        let newMessage = "You won! Press Reset to play again!"
+        setState({...state, message: newMessage, win: true})
+        return state.win;
+    } else if (evalBoard.includes('o,o,o')) {
+        let newMessage = "You let a dumb computer beat you. That's embarrassing. If your ego can handle it, press Reset to play again."
         console.log("bot")
-    } else {
-      if (checkIfFull()) {
-        let newMessage = "Awww, it's a draw. Too bad. Press RESET to play again... maybe you'll actually win this time."
+        setState({...state, message: newMessage, win: true})
+        return state.win;
+    } else if (checkIfFull()) {
+        let newMessage = "Awww, it's a draw. Too bad. Press Reset to play again... Maybe you'll actually win this time."
         setState({...state, message: newMessage})
         console.log("full -- draw")
-      }
-      else {
-        console.log("not full -- bot's turn")
-        handleBotTurn();
-      }
+        return state.win;
+    } else {
+      return state.win;
     }
   }
 
@@ -100,7 +102,7 @@ function App() {
     </header>
     <main>
       <ul className="board">
-        {state.board.map((pawn, index) => <li key={index} data-key={index} className='xo' onClick={handleUserTurn}>{pawn}</li>)}
+        {state.board.map((pawn, index) => <li key={index} data-key={index} className='xo' disabled={pawn === '' && !state.win ? false : true } onClick={handleUserTurn}>{pawn}</li>)}
       </ul>
       <form>
         <button className="btn-prim btn" type="button">
